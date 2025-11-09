cf5d51a87d892d85bb41cb3b738678f58806a603
lucasvrcc <lucasvalentim@soulasalle.com.br>
2025-11-09 16:58:04 -0300

    Temperatura teste

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // JavaScript source code
+
+//Temperatura
+
+function converter() {
+    let c = Number(document.getElementById('c').value);
+    let f = (c * 9 / 5) + 32;
+    document.getElementById('resultado').innerText = c + "°C = " + f.toFixed(2) + "°F";
+}
